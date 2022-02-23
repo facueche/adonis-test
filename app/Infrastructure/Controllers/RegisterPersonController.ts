625f2d21c33fa778fb3d6349873305a2d5dd145b
Facu Echenique <facueche@gmail.com>
2022-02-23 20:14:40 -0300

    wip: implements infrastructure of persona registration

--- app/Infrastructure/Controllers/RegisterPersonController.ts
+++ app/Infrastructure/Controllers/RegisterPersonController.ts
@@ -1,9 +1,21 @@
+import RegisterPersonAction from "App/Application/RegisterPersonAction";
+import PersonRepository from "../Repositories/PersonRepository";
+
 export default class RegisterPersonController
 {
     public handle = async (request) => {
-        let data = request.body();
+        const data = request.body();
+        const personRepository: PersonRepository = new PersonRepository();
 
-        let name: string = data.name;
-        let age: number = data.age;
+        const uuid: string = personRepository.generateUuid();
+        const name: string = data.name;
+        const age: number = data.age;
+
+        const action: RegisterPersonAction = new RegisterPersonAction();
+        action
+            .setUuid(uuid)
+            .setName(name)
+            .setAge(age)
+            .handle();
     }
 }
--- app/Infrastructure/Repositories/PersonRepository.ts
+++ app/Infrastructure/Repositories/PersonRepository.ts
@@ -1,6 +1,8 @@
+import {v4 as uuid} from 'uuid';
+
 export default class PersonRepository
 {
     public generateUuid = (): string => {
-        return '';
+        return uuid();
     }
 }
